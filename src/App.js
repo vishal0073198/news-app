2915504c398d7e10474fa7c4e37b05110f9cef2a
vishal0073198 <vishal0073198@gmail.com>
2024-11-27 11:08:25 +0530

    Adding Headers

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
      <h1 className='bg-red-300'>Hello</h1>
+     <Header />
     </div>
   );
 }
